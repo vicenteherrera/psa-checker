0e0d94eaf2a34864f451c6721a7e7d00fe9d7bb9
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2022-11-12 20:12:52 +0100

    docs: removing unnecesary files for the docs branch

--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import cmd "github.com/vicenteherrera/psa-checker/cmd/psa-checker"
-
-func main() {
-	cmd.Execute()
-}
--- pkg/analyzer/model.go
+++
@@ -1,9 +0,0 @@
-package analyzer
-
-import "time"
-
-type AnalyzerResponse struct {
-	AnalysisStatus string    `json:"analysis_status"`
-	CreatedAt      time.Time `json:"created_at"`
-	Allowed        bool      `json:"allowed"`
-}
