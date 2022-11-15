c0a579815e9acd12820bebee3ce71362a758d39a
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2022-11-15 08:38:52 +0100

    added badrobot information and usage on pipeline

--- util/hub_eval/src/write_table.py
+++ util/hub_eval/src/write_table.py
@@ -35,7 +35,15 @@ count = {
     "error_download":0,
     "error_template":0,
 }
+
 brbuckets = {}
+br_min_buckets = -700
+br_size_buckets = -50
+br_n_buckets = int (br_min_buckets / br_size_buckets)
+brmin = br_non_evaluable = br_no_workload = 0
+for i in range(0, br_min_buckets, br_size_buckets):
+    brbuckets[str(i)] = 0
+
 calpha = {}
 for l in ascii_lowercase:
     calpha[l]=0
@@ -52,40 +60,58 @@ for key in keys_pss:
         if dic_chart["pss"]["date"] > date:
             date = dic_chart["pss"]["date"]
         level = dic_chart["pss"]["level"]
+
         if "score_badrobot" in dic_chart["pss"]:
-            if dic_chart["pss"]["score_badrobot"] in brbuckets:
-                brbuckets[dic_chart["pss"]["score_badrobot"]] += 1
-            else:
-                brbuckets[dic_chart["pss"]["score_badrobot"]] = 1
+            score = dic_chart["pss"]["score_badrobot"]
+            if score == "":
+                br_non_evaluable += 1
+            elif dic_chart["pss"]["level"] in ["empty_no_object", "no_pod_object_but_crd", "no_pod_object"]:
+                br_no_workload += 1
+            else :
+                score = int(score)
+                if score < brmin: brmin = int(score)
+                for i in range(0, br_min_buckets + br_size_buckets, br_size_buckets):
+                    if i >= score and score > ( i + br_size_buckets ):
+                        brbuckets[str(i)] += 1
+                        break
+
     count["total"] +=1
     if level not in count:
-        count [level] = 1
+        count[level] = 1
     else:
         count[level] += 1
-    print( "# ["+ str(i) + "/" + str(len(charts_pss))+ "] " + dic_chart["repository"]["name"] + level)
+    # print( "# ["+ str(i) + "/" + str(len(charts_pss))+ "] " + dic_chart["repository"]["name"] + level)
 
 date = "Evaluation date: " + date + "\n"
-list_md.write(header + date + "\n### Pod Security Standards (PSS)\n\n")
+list_md.write(header + "Source: [Artifact Hub](https://artifacthub.io/)  \n" + date + "\n### Pod Security Standards (PSS)\n\n")
+list_md.write("[Pod Security Standards (PSS)](https://kubernetes.io/docs/concepts/security/pod-security-standards/) define three levels of security (restricted, baseline and privileged) that can be enforced for pods in a namespace. Evaluation done with [psa-checker](https://vicenteherrera.com/psa-checker/) command line tool, that checks into Kubernetes objects that can create pods.\n\n")
 list_md.write("| Category | Quantity | Percentage |\n|------|------|------|\n")
 for key in count.keys():
     list_md.write("| " + key.capitalize() + " | " + str(count[key]) + " | ")
     list_md.write( str(round(100*count[key]/count["total"],2))+"% |\n" )
 list_md.write("\n")
 list_md.write("Legend:\n")
-list_md.write(" * Privileged, Baseline, Restricted: doesn’t account CRDs that could create pods\n")
-list_md.write(" * Error_download: Downloading the template from original source wasn’t possible\n")
-list_md.write(" * Error_template: Rendering the template without providing parameters resulted in error\n")
-list_md.write(" * No_pod_object_but_crd: The chart didn’t render any object that can create pods, but has CRD that could do so\n")
-list_md.write(" * No_pod_object_no_crd: The chart didn’t render any object that can create pods nor CRDs\n")
-list_md.write(" * Version_not_evaluable: The cart includes deployment, daemonset, etc. of v1beta1 that can’t be evaluated by the library\n")
+list_md.write(" * PSS level:\n")
+list_md.write("   * Privileged: Pod specs makes use of privileged settings, the most insecure. Containers are able to access host capabilities.\n")
+list_md.write("   * Baseline: Pod specs without extra security or extra privileges. Doesn't account for CRD that may create pods.\n")
+list_md.write("   * Restricted: Pod specs follow the best security practices, like requiring containers to not run as root, and drop extra capabilities. Doesn't account for CRDs that may create pods.\n")
+list_md.write(" * Error_download: Downloading the template from original source wasn't possible.\n")
+list_md.write(" * Error_template: Rendering the template without providing parameters resulted in error.\n")
+list_md.write(" * No_pod_object_but_crd: The chart didn't render any object that can create pods, but has CRD that could do so.\n")
+list_md.write(" * No_pod_object_no_crd: The chart didn't render any object that can create pods nor CRDs.\n")
+list_md.write(" * Version_not_evaluable: The cart includes deployment, daemonset, etc. of v1beta1 that can't be evaluated by the library.\n")
 list_md.write("\n")
 
-list_md.write("### BadRobot evaluation buckets\n\n")
-list_md.write("[BadRobot](https://github.com/controlplaneio/badrobot) scores how secure Kubernetes operators are.\n")
+list_md.write("### Operator evaluation with BadRobot score\n\n")
+list_md.write("[BadRobot](https://github.com/controlplaneio/badrobot) evaluates how secure Kubernetes operators are. For each operator included in a chart, a score is calculated with a set of security practices. The closer to zero the score, the better.\n\n")
+# list_md.write("Worse score: " + str(brmin)+"\n\n")
 list_md.write("| Score | Number of charts |\n|------|------|\n")
+list_md.write("| Non-evaluable | " + str(br_non_evaluable) + " |\n")
+list_md.write("| No workload | " + str(br_no_workload) + " |\n")
+
 keys = brbuckets.keys()
 for key in keys:
-    list_md.write("| " + str(key) + " | " + str(brbuckets[key]) + " |\n")
+    list_md.write("| [" + str(key) + ", " + str(int(key) + br_size_buckets) + ") | " + str(brbuckets[key]) + " |\n")
 
 
 list_md.write("\n### Charts list\n\n")
@@ -114,11 +140,11 @@ for key in keys_pss:
 
     if letter != last_letter:
         last_letter = letter
-        print("# Writing header: "+letter)
+        # print("# Writing header: "+letter)
         list_md.close()
         list_md = open(doc_filename+"_"+letter+".md", "w")
         list_md.write(header + date + "\n" + index)
-        list_md.write("\n\n| repo | chart | PSS level | BR score | chart version | app version |\n")
+        list_md.write("\n\n| repo | chart | PSS level | BadRobot score | chart version | app version |\n")
         list_md.write("|------|------|------|------|------|------|\n")
 
     level = ""
@@ -127,7 +153,7 @@ for key in keys_pss:
         level = dic_chart["pss"]["level"]
         if "score_badrobot" in dic_chart["pss"]:
             brscore = dic_chart["pss"]["score_badrobot"]
-    print( "# ["+ str(i) + "/" + str(len(keys_pss))+ "] " + repo + " " + chart + " " + level)
+    # print( "# ["+ str(i) + "/" + str(len(keys_pss))+ "] " + repo + " " + chart + " " + level)
     list_md.write("| [" + repo + "](" + url + ") | " + chart + " | " + level  + " | " + brscore  + " | " + version + " | " + app_version  + " |\n")
     # sys.exit()
 
