d0d3c6f269ba98ea89a0989b1d237923e6dca71e
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2022-11-29 20:58:54 +0100

    feat: added ldflags build settings with version information

--- cmd/psa-checker/root.go
+++ cmd/psa-checker/root.go
@@ -13,8 +13,6 @@ import (
 
 var cfgFile string
 
-var version = "0.0.1"
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "psa-checker",
---
+++ cmd/psa-checker/version.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// Based on: https://github.com/controlplaneio/badrobot/blob/master/cmd/version.go
+
+var (
+	// vars injected with ldflags at build time (this can be done automatically by goreleaser)
+	version = "unknown"
+	commit  = "unknown"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   `version`,
+	Short: "Prints badrobot version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("version %s\ngit commit %s\n", version, commit)
+	},
+}
