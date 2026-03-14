b2f488464bac48f80bcd4b1d42c16a4d4492edbe
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2026-03-14 18:54:47 +0100

    fix: file not found returns right error message

--- test/test-fail.sh
+++ test/test-fail.sh
@@ -6,5 +6,12 @@ cd ../release
 [ $? -ne 1 ] && exit 1
 ./psa-checker --level restricted --filename ../test/pod-baseline.yaml 1>/dev/null 2>/dev/null
 [ $? -ne 1 ] && exit 1
+./psa-checker --level restricted --filename ../test/pod-baseline.yaml 1>/dev/null 2>/dev/null
+[ $? -ne 1 ] && exit 1
+
+# Test for non-existent file
+output=$(./psa-checker --filename nonexistent.yaml 2>&1)
+[ $? -ne 1 ] && exit 1
+if [[ "$output" != *"no such file or directory"* ]]; then exit 1; fi
 
 echo "[ success ] Non compliant manifests succesfully flagged"
